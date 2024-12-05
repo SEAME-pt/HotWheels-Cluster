90d7cdbbc3a178ccd4ce4d5afe83907994568b28
Tiago Pereira <tiagosodrepereira@gmail.com>
2024-12-05 15:05:00 +0000

    documentation of the functions done

--- car_controls/main.cpp
+++ car_controls/main.cpp
@@ -8,11 +8,29 @@
 volatile bool keepRunning = true;
 
 // Signal handler function
+/**
+ * Signal handler that sets the `keepRunning` flag to false when a termination signal is received.
+ * 
+ * - `signum`: The signal number (e.g., SIGTERM, SIGINT).
+ * 
+ * This function prints a message indicating the program has been stoped,
+ * and stops the running process by setting `keepRunning` to false.
+ */
 void signalHandler(int signum) {
     std::cout << "Signal (" << signum << ") received. Stopping gracefully..." << std::endl;
     keepRunning = false;
 }
 
+/**
+ * Main function that initializes and controls the Jetcar.
+ * 
+ * - Sets up signal handlers for termination signals (SIGTERM and SIGINT).
+ * - Creates a Jetcar object with the specified servo and motor I2C addresses.
+ * - Starts the Jetcar and continuously runs until a termination signal is received.
+ * - Stops the Jetcar and handles any exceptions that may occur during initialization or operation.
+ * 
+ * The function ensures proper cleanup before exiting and prints an error message if an exception is thrown.
+ */
 int main() {
     signal(SIGTERM, signalHandler);
     signal(SIGINT, signalHandler);
